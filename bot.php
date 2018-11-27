4e17c8fcafa3682e6b0dd1b28c28cc2d6626e1bc
Ittirit Thipphinit <itchampclub@gmail.com>
2018-11-27 16:01:08 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,67 +1,191 @@
 <?php
-$strAccessToken = "1B8pMUtZdLgdebgTuRrxV3YirCQv91mbXGnxvlTbX7Cxn471Fs0bBgwGVpedxnPKm7tZUWxnMrT2NqCBCLAG8L7r6vtYoZwb3iqRvYr3BZGrZX/mRNFG8lzNbLr5CHO4PWfTicerD5PVHYjC8mpQ4wdB04t89/1O/w1cDnyilFU=";
-$content = file_get_contents('php://input');
-$arrJson = json_decode($content, true);
-$strUrl = "https://api.line.me/v2/bot/message/reply";
-$_userId = $arrJson['events'][0]['source']['userId'];
-$_groupId = $arrJson['events'][0]['source']['groupId'];
-$_msg = $arrJson['events'][0]['message']['text'];
-$arrHeader = array();
-$arrHeader[] = "Content-Type: application/json";
-$arrHeader[] = "Authorization: Bearer {$strAccessToken}";
+require_once('./LineBotTiny.php');
+$channelAccessToken = '1B8pMUtZdLgdebgTuRrxV3YirCQv91mbXGnxvlTbX7Cxn471Fs0bBgwGVpedxnPKm7tZUWxnMrT2NqCBCLAG8L7r6vtYoZwb3iqRvYr3BZGrZX/mRNFG8lzNbLr5CHO4PWfTicerD5PVHYjC8mpQ4wdB04t89/1O/w1cDnyilFU=';
+$channelSecret = '69b2d81ee6e8ff48d2cacdc8c7d8c337';
+date_default_timezone_set('Asia/Jakarta');
 
-if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
-  $arrPostData = array();
-  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-  $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = "สวัสดี ID ของคุณคือ ".$arrJson['events'][0]['source']['userId'];
+$client 		= new LINEBotTiny($channelAccessToken, $channelSecret);
+$reply 			= '';
+$leave 			= false;
+
+$event 			= $client->parseEvents()[0];
+$type 			= $event['type']; 
+$source     	= $event['source'];
+$userId 		= $source['userId'];
+$replyToken 	= $event['replyToken'];
+$timestamp		= $event['timestamp'];
+$message 		= $event['message'];
+$messageid 		= $message['id'];
+
+
+
+
+
+if($source['type'] == "group") {
+			$userData = $client->getProfilFromGroup($userId, $source['groupId']);
+		}
+		else if($source['type'] == "room") {
+			$userData = $client->getProfilFromRoom($userId, $source['roomId']);
+		}
+		else if($source['type'] == "user") {
+			$userData = $client->profil($userId);
+		}
+
+
+
+
+
+
+
+if($type == 'memberJoined') 
+{
+	$replyText = "Hi";
 	
-}else if($arrJson['events'][0]['message']['text'] == "Turnleft"){
-  $arrPostData = array();
-  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-  $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = "Bot : เลี้ยวซ้าย";
-	
-}else if($arrJson['events'][0]['message']['text'] == "Turnright"){
-  $arrPostData = array();
-  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-  $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = "Bot : เลี้ยวขวา";
-	
-}else if($arrJson['events'][0]['message']['text'] == "Forback"){
-  $arrPostData = array();
-  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-  $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = "Bot : ถอยหลัง";
-	
-}else if($arrJson['events'][0]['message']['text'] == "Forward"){
-  $arrPostData = array();
-  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-  $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = "Bot : ตรงไปข้างหน้า";
-	
-}else if($arrJson['events'][0]['message']['text'] == "Stop"){
-  $arrPostData = array();
-  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-  $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = "Bot : หยุด";
-	
-}else{
-  $arrPostData = array();
-  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-  $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = "Bot :ไม่เข้าใจคำสั่ง";
+	$reply = array(
+								'replyToken' => $replyToken,														
+								'messages' => array(
+									array(
+											'type' => 'text',					
+											'text' => $replyText
+										)
+								)
+							);
+
 }
- 
- 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL,$strUrl);
-curl_setopt($ch, CURLOPT_HEADER, false);
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
-curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-$result = curl_exec($ch);
-curl_close ($ch);
- ?>
+else if($type == 'join') 
+{
+	$replyText = 'Hello';
+	
+	$reply = array(
+								'replyToken' => $replyToken,														
+								'messages' => array(
+									array(
+											'type' => 'text',					
+											'text' => $replyText
+										)
+								)
+							);
+
+}
+else if($message['type']=='text')
+{
+	$incomingMsg = strtolower($message['text']);
+	if(strpos($incomingMsg,"botleave") !== false)
+        {
+	        $replyText = '';
+		$reply = array(
+								'replyToken' => $replyToken,														
+								'messages' => array(
+									array(
+											'type' => 'text',					
+											'text' => $replyText
+										)
+								)
+							);
+		$leave = true;
+        }
+	else if(strpos($incomingMsg,"flexmsg") !== false)
+		{
+		$replyText = 'ตารางสอบวิชา 14215 คือ วันอาทิตย์ ที่ 27 มกราคม 2561 เวลา 13.30-16.30 น.'.$userData['displayName'];
+		$reply = array(
+								'replyToken' => $replyToken,														
+								'messages' => array(
+									array(
+
+    'type' =>  'flex',
+    'altText' =>  'This is a Flex message',
+    'contents' =>  (
+(
+  'type' =>  'bubble',
+  'header' =>  (
+    'type' =>  'box',
+    'layout' =>  'vertical',
+    'contents' =>  [
+      (
+        'type' =>  'text',
+        'text' =>  'header'
+      )
+    ]
+  ),
+  'hero' =>  (
+    'type' =>  'image',
+    'url' =>  'https => //www.linefriends.com/img/img_sec.jpg',
+    'size' =>  'full',
+    'aspectRatio' =>  '2 => 1'
+  ),
+  'body' =>  (
+    'type' =>  'box',
+    'layout' =>  'vertical',
+    'contents' =>  [
+      (
+        'type' =>  'text',
+        'text' =>  'body'
+      ),
+	  (
+        'type' =>  'text',
+        'text' =>  'body'
+      )
+    ]
+  ),
+  'footer' =>  (
+    'type' =>  'box',
+    'layout' =>  'vertical',
+    'contents' =>  [
+      (
+        'type' =>  'text',
+        'text' =>  'footer'
+      )
+    ]
+  )
+)
+
+    )
+)
+										)
+								
+							);
+	       }
+	else if(strpos($incomingMsg,"groupid") !== false)
+	{
+		$userData = null;
+		if($source['type'] == "group") {
+			$userData = $client->getProfilFromGroup($userId, $source['groupId']);
+		}
+		else if($source['type'] == "room") {
+			$userData = $client->getProfilFromRoom($userId, $source['roomId']);
+		}
+		else if($source['type'] == "user") {
+			$userData = $client->profil($userId);
+		}
+		
+		if($userData != null) {
+			$replyText = "Hi ".$source['groupId'];
+			$reply = array(
+							'replyToken' => $replyToken,														
+							'messages' => array(
+								array(
+										'type' => 'text',					
+										'text' => $replyText
+									)
+							)
+						);
+		}
+	}
+}
+
+
+if($reply != "") {
+				
+		$client->replyMessage($reply);
+	 
+	 	if($leave) {
+	 		if($source['type'] == "group") {
+				$client->leaveGroup($source['groupId']);
+			}
+			else if($source['type'] == "room") {
+				$client->leaveRoom($source['roomId']);
+			} 
+	 	}	
+}
+?>
+        
